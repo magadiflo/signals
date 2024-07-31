8414aa15ec55f9d1ad1c812399d0e53df889c35a
Martín <magadiflo@gmail.com>
2024-07-30 20:09:13 -0500

    Crea un counter signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
   standalone: true,
   imports: [],
   templateUrl: './counter-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class CounterPageComponent {
+  public counter = signal<number>(10);
+  public squareCounter = computed<number>(
+    () => this.counter() * this.counter()
+  );
 
+  public increaseOrDecreaseBy(value: number) {
+    this.counter.update((currentValue) => currentValue + value);
+  }
+
+  public resetCounter() {
+    this.counter.set(0);
+  }
 }
