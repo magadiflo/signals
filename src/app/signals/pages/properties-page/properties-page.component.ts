fe8b4e6d87f8c66b3c156f03a35de08891c9d0e3
Martín <magadiflo@gmail.com>
2024-07-31 13:06:42 -0500

    Actualiza atributos

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './properties-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 2,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg',
+  });
 
-  public onFieldUpdated(field: string, value: string) {
-    console.log({field, value});
+  public onFieldUpdated(field: keyof User, value: string) {
+    // this.user.set({ ...this.user(), [field]: value });
+    // this.user.update((currentUser) => ({ ...currentUser, [field]: value }));
+
+    this.user.update((currentUser) => {
+      switch (field) {
+        case 'email':
+          currentUser.email = value;
+          break;
+        case 'first_name':
+          currentUser.first_name = value;
+          break;
+        case 'last_name':
+          currentUser.last_name = value;
+          break;
+        case 'avatar':
+          currentUser.avatar = value;
+          break;
+        case 'id':
+          currentUser.id = Number(value);
+          break;
+      }
+      return currentUser;
+    });
   }
-
 }
