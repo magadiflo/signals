b88e7c34677d59b942da80cc3554e3afc221217a
Martín <magadiflo@gmail.com>
2024-07-30 19:38:38 -0500

    Crea menú lateral

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { MenuItem } from '../../interfaces/menu.interface';
+
+@Component({
+  selector: 'side-menu',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu.component.html',
+  styles: `
+    li {
+      cursor: pointer;
+    }
+  `,
+})
+export class SideMenuComponent {
+  public menuItems: MenuItem[] = [
+    { title: 'Contador', route: 'counter' },
+    { title: 'Info. Usuario', route: 'user-info' },
+    { title: 'Mutaciones', route: 'properties' },
+  ];
+}
--- src/app/signals/layout/signals-layout/signals-layout.component.ts
+++ src/app/signals/layout/signals-layout/signals-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
+
 @Component({
   selector: 'app-signals-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: './signals-layout.component.html',
   styles: ``
 })
